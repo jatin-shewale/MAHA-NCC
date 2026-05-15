f4035bf8b70299566cf815a2b79419ed4d4a864c
jatin-shewale <shewalejatin427@gmail.com>
2026-05-15 10:14:13 +0530

    Initial Commit

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,6 @@ import { env } from "./env.js";
 export const connectDB = async () => {
   mongoose.set("strictQuery", true);
   await mongoose.connect(env.mongoUri);
+  console.log("MongoDB connected");
   return mongoose.connection;
 };
